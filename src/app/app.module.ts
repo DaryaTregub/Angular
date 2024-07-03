c65a9d19824800e39ece3fb9c1a2fc9deb40a307
TregubDarya <Tregub.Darya@mfc-chita.ru>
2024-07-03 16:59:30 +0900

    1.2 title and counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      CounterComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
