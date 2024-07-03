c2c3e32abb2a1e9100e45d264cd8a880c2c605ee
TregubDarya <Tregub.Darya@mfc-chita.ru>
2024-07-03 17:04:01 +0900

    +files

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+})
+export class CounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  counter = 0;
+
+  changeCount(type: boolean) {
+    type ? this.counter++ : this.counter--
+  }
+
+}
