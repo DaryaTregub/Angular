80c2729c0c306c8dc53c130ceb33c134bd4187c4
TregubDarya <Tregub.Darya@mfc-chita.ru>
2024-07-11 16:35:18 +0900

    задание 1.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarsComponent } from './cars/Cars.component';
+import { DatePipeComponent } from './date/date-pipe.component';
+import { CounterComponent } from './counter/counter.component';
+import { CounterPipe } from './counter.pipe';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [				
+    AppComponent,
+      CarsComponent,
+      DatePipeComponent,
+      CounterComponent,
+      CounterPipe
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
