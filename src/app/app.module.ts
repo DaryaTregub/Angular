92d50cc067398222ba55c7a6b6d787f024e04d30
TregubDarya <Tregub.Darya@mfc-chita.ru>
2024-07-29 12:55:03 +0900

    установлен bootstrap, ngxs, добавлены основные модули и компоненты

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ErrorPageComponent } from './error/error.component';
+import { AutorizationComponent } from './autorization/autorization.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent,
+    AutorizationComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
